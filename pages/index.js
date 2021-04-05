20bfb461f62ea0ff905f0fd81632815ee0abd9b1
joenapper <joe.napper@hotmail.co.uk>
2021-04-05 13:41:57 +0100

    Raw data functionality in place

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 import Head from "next/head";
+import Link from "next/link";
+import { blogPosts } from "../lib/data";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Blogs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1>My Blogs!</h1>
       </main>
+
+      <div>
+        {blogPosts.map((item) => (
+          <div key={item.slug}>
+            <div>
+              <Link href={`/blog/${item.slug}`}>
+                <a>{item.title}</a>
+              </Link>
+            </div>
+            <div>{item.date.toString()}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
